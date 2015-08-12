62cae2eac22323b97f1b3d73403d5e869da2114f
serred03 <edse.3103@gmail.com>
2015-08-11 22:29:34 -0500

    Added working cached program

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+#Creates a "matrix" object capable of storing values on different lexical scopes. 
 makeCacheMatrix <- function(x = matrix()) {
 
+        inv <- NULL 
+        
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        
+        get <- function()x
+        setinverse <- function(inverse) inv <<- inverse
+        getinverse <- function() inv
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+#Solves the inverse of a matrix if there is no cached value. 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinverse() 
+        
+        if(!is.null(inv)){
+                message("getting cached data ")
+                return(inv)
+        }
+        
+        data <- x$get()
+        
+        inverse <- solve(data)
+        x$setinverse(inverse)
+        inverse
+        
 }
